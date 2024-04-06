603cf9cafaf0dce311001fe19fe1047c8a41de24
Kah Ho <sweekahho.gmail.com>
2024-04-06 19:29:30 +0800

    create webpage template

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,170 @@
 import { useState } from 'react'
-import reactLogo from './react.svg'
-import './Home.css'
-import { Flex, Text, Image, Button } from '@chakra-ui/react'
+import { Flex, Text, Button, background } from '@chakra-ui/react'
+import { AttachmentIcon, EditIcon, DownloadIcon } from '@chakra-ui/icons'
 
 const Home: React.FC = () => {
-	const [count, setCount] = useState(0)
+	const [riskMap, setRiskMap] = useState(false)
 
 	return (
 		<Flex
 			height="100vh"
-			flexDir="column"
+			flexDir="row"
 			alignItems="center"
-			paddingTop="40px"
+			bgColor="#F4F4F4"
 		>
-			<Flex flexDir="row">
-				<Image
-					src={reactLogo}
-					boxSize="160px"
-					className="logo"
-					id="react-logo"
-				/>
+			{/* Left bar */}
+			<Flex
+				flexDir="column"
+				height="100%"
+				width="25vw"
+				bgColor="#9786FF"
+				padding="30px 12px"
+				justifyContent="space-between"
+			>
+				{/* Logo */}
+				<Text fontSize="2xl" fontWeight="bold" alignSelf="center">
+					QuickSave
+				</Text>
+				{/* Location List */}
+				<Flex
+					marginTop="40px"
+					flexDir="column"
+					height="100%"
+					gap="10px"
+				>
+					<Text fontSize="xl" fontWeight="semibold">
+						Key Areas
+					</Text>
+					<Flex
+						height="60%"
+						flexDir="column"
+						bgColor="white"
+						borderRadius="12px"
+						padding="10px"
+						gap="5px"
+					>
+						{/* Data Rows */}
+						<Flex
+							flexDir="row"
+							borderRadius="8px"
+							padding="6px 12px"
+							bgColor="red"
+							justifyContent="space-between"
+						>
+							<Text>Lagoon Hawker Center</Text>
+							<Text>92</Text>
+						</Flex>
+						<Flex
+							flexDir="row"
+							borderRadius="8px"
+							padding="6px 12px"
+							bgColor="orange"
+							justifyContent="space-between"
+						>
+							<Text>HDB Block 10</Text>
+							<Text>64</Text>
+						</Flex>
+					</Flex>
+				</Flex>
+				{/* Risk Map Toggle */}
+				<Button
+					height="52px"
+					width="100%"
+					bgColor={riskMap ? '#454545' : 'white'}
+					color={riskMap ? 'white' : 'black'}
+					_hover={
+						riskMap
+							? { bgColor: 'darkgray' }
+							: { bgColor: 'lightgray' }
+					}
+					onClick={() => setRiskMap((riskMap) => !riskMap)}
+				>
+					<Text display={riskMap ? 'initial' : 'none'}>
+						Disable Risk Map
+					</Text>
+					<Text display={riskMap ? 'none' : 'initial'}>
+						Enable Risk Map
+					</Text>
+				</Button>
 			</Flex>
-			<Text fontSize="xl" margin="10px 0 20px 0">
-				Test vite hot reloading feature: {count}
-			</Text>
-			<Button onClick={() => setCount((count) => count + 1)}>
-				Click Me
-			</Button>
+			{/* Main Panel */}
+			<Flex flexDir="column" width="100%" height="100%" padding="16px">
+				{/* Utilities Section */}
+				<Flex
+					flexDir="row"
+					alignItems="center"
+					justifyContent="space-between"
+					paddingBottom="12px"
+				>
+					<Flex
+						flexDir="row"
+						bgColor="#FFF"
+						height="65%"
+						width="500px"
+						borderRadius="30px"
+						padding="0px 18px"
+						alignItems="center"
+						boxShadow="md"
+					>
+						<Text fontSize="xl" fontWeight="semibold">
+							Google Maps URL:
+						</Text>
+					</Flex>
+					{/* Functional Buttons */}
+					<Flex flexDir="row" gap="20px" marginRight="30px">
+						<Flex flexDir="column" alignItems="center">
+							<Button
+								bgColor="white"
+								border="2px solid #034AFF"
+								borderRadius="30px"
+								height="50px"
+								width="50px"
+								padding="10px"
+								boxShadow="md"
+							>
+								<EditIcon />
+							</Button>
+							<Text fontSize="md">DRAW</Text>
+						</Flex>
+						<Flex flexDir="column" alignItems="center">
+							<Button
+								bgColor="white"
+								border="2px solid #034AFF"
+								borderRadius="30px"
+								height="50px"
+								width="50px"
+								padding="10px"
+								boxShadow="md"
+							>
+								<AttachmentIcon />
+							</Button>
+							<Text fontSize="md">IMPORT</Text>
+						</Flex>
+						<Flex flexDir="column" alignItems="center">
+							<Button
+								bgColor="white"
+								border="2px solid #034AFF"
+								borderRadius="30px"
+								height="50px"
+								width="50px"
+								padding="10px"
+								boxShadow="md"
+							>
+								<DownloadIcon />
+							</Button>
+							<Text fontSize="md">EXPORT</Text>
+						</Flex>
+					</Flex>
+				</Flex>
+				{/* Google Maps */}
+				<Flex
+					height="100%"
+					width="100%"
+					bgColor="white"
+					borderRadius="12px"
+					boxShadow="md"
+				></Flex>
+			</Flex>
 		</Flex>
 	)
 }
